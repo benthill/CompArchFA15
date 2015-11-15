d59a5dceaa7e8957de72f5cee4f86089c442befe
Isaac Getto <isaac.getto@gmail.com>
2015-11-15 16:46:38 -0500

    add KaiIsaacDavid assembly

---
+++ Lab/Lab3/asmtest/KaiIsaacDavid/division.asm
@@ -0,0 +1,44 @@
+#-------------------------------------------------------
+# Divide
+# 
+# This function divides opA by opB.
+#
+# The result of the integer division is put in 
+# register $v0 and the remainder is put in register $v1.
+#
+# The operand values can be changed in the .data section.
+#--------------------------------------------------------
+
+# Load operands into registers
+lw $t3, one
+lw $a0, opA
+lw $a1, opB
+
+# Setup result registers
+add $t2, $zero, $zero
+add $t0, $zero, $a0
+
+LOOP_START:
+
+    slt $t1, $t0, $a1
+    bne $t1, 0, LOOP_END
+
+    sub $t0, $t0, $a1
+    add $t2, $t2, $t3
+
+j LOOP_START
+
+
+LOOP_END:
+
+# Write results to registers
+add $v0, $t2, $zero
+add $v1, $t0, $zero
+
+# Modify opA and opB to compute different quotients
+.data
+	one: .word 1
+	opA: .word 11
+	opB: .word 5
+
+
