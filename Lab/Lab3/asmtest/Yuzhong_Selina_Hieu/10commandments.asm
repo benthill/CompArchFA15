465e8954aef44e65e01501304785748815a80b85
Computer Architecture Fall 2015 <comparch15@gmail.com>
2015-11-16 11:46:03 -0500

    Updated README and comments for  Yuzhong, Selina, and Hieu

--- Lab/Lab3/asmtest/Yuzhong_Selina_Hieu/10commandments.asm
+++ Lab/Lab3/asmtest/Yuzhong_Selina_Hieu/10commandments.asm
@@ -1,22 +1,24 @@
-add $a0, $zero, 6
-add $a1, $zero, 8
-xori $v1, $a0, 1 #random result
-slt $v0, $a0, $v1 #random result
+# Selina, Yuzhong, Hieu
 
-jal FIRST_STEP
-j END
+add $a0, $zero, 6 # Set a0
+add $a1, $zero, 8 # Set a1
+xori $v1, $a0, 1 #random result. Should be 7
+slt $v0, $a0, $v1 #random result. Should be 1
+
+jal FIRST_STEP # Jumps to 9
+j END # Jumps to 21
 
 FIRST_STEP:
-	sub $sp, $sp, $a1
+	sub $sp, $sp, $a1 # Move stack pointer to add new data
 	sw $a0, 4($sp)
 	sw $ra, 0($sp)
-	bne $a0, $a1, NEXT_STEP
+	bne $a0, $a1, NEXT_STEP # a0 and a1 should not be the same (6 and 8). Branches to 15
 	
 NEXT_STEP:
-	lw $t1, 4($sp)
+	lw $t1, 4($sp) # Loading data saved to stack
 	lw $ra, 0($sp)
 	add $sp, $sp, $a1
-	jr $ra
+	jr $ra #Jumps to end of line 6
 	
 END: 
 	syscall
---
+++ Lab/Lab3/asmtest/Yuzhong_Selina_Hieu/README.md
@@ -0,0 +1,15 @@
+## Expected results
+
+After you run the program:
+- $v0 is 1
+- $v1 is 7
+- $a0 is 6
+- $a1 is 8
+
+## Memory layout requirements
+
+- Needs one byte available beneath the stack pointer.
+
+## Instructions used outside of the 10 required commands
+
+- None
